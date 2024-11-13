e6d5fee3d0195e2fc8faaa9f1c5802f9017af56a
Gardasvara <alovegardasvara@gmail.com>
2024-11-13 23:18:37 +0700

    update dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -33,7 +33,7 @@ with col1:
         x="revenue",
         y="customer_state",
         data=state_sales_df.sort_values(by="revenue", ascending=False),
-        palette="hls"
+        palette="greys"
     )
     plt.title("Total Revenue per State", loc="center", fontsize=14)
     plt.ylabel(None)
@@ -47,7 +47,7 @@ with col2:
         x="order_count",
         y="customer_state",
         data=state_sales_df.sort_values(by="order_count", ascending=False),
-        palette="hls"
+        palette="greys"
     )
     plt.title("Total Number of Orders per State", loc="center", fontsize=14)
     plt.ylabel(None)
@@ -102,7 +102,7 @@ sns.barplot(
     y="total_sales",
     hue="order_purchase_timestamp",
     data=monthly_sales_df,
-    palette="cubehelix",  
+    palette="blues",  
     legend=False,
     ax=axs[0]
 )
@@ -117,7 +117,7 @@ sns.barplot(
     y="order_count",
     hue="order_purchase_timestamp",
     data=monthly_sales_df,
-    palette="viridis",  
+    palette="blues",  
     legend=False,
     ax=axs[1]
 )
